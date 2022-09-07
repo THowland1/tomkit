30949bef537e9dff4abac1cae327933289a5fa13
Tom Howland <tomhowland9@gmail.com>
2022-09-07 17:58:37 +0100

    Maybe fix netlify build

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,14 +1,15 @@
 <script lang="ts">
 	import { goto } from '$app/navigation';
-
-	goto('/base-converter');
+	import { browser } from '$app/environment';
+	if (browser) {
+		goto('/base-converter');
+	}
 </script>
 
 <svelte:head>
 	<title>Home</title>
 	<meta name="description" content="Svelte demo app" />
 </svelte:head>
-<section class="section">d</section>
 
 <style lang="scss">
 </style>
